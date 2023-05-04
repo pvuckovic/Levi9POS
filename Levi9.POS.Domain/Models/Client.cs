eb2ae0f54318c5fa90dc51c0e0b20596f6b9aa6a
Stevan <61841410+AleteiaSD@users.noreply.github.com>
2023-05-04 05:33:06 -0700

    DataBase initialization 2

---
+++ Levi9.POS.Domain/Models/Client.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levi9.POS.Domain.Models
+{
+    
+    public class Client
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Guid GlobalId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [StringLength(255)]
+        public string Address { get; set; } = string.Empty;
+        [Required]       
+        [StringLength(150)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; } = string.Empty;
+        [Required]
+        [StringLength(18,MinimumLength =18)]
+        public string LastUpdate { get; set; } = string.Empty;
+
+
+        public List<Document> Documents { get; set; } = new List<Document>();
+    }
+}
---
+++ Levi9.POS.Domain/Models/ProductDocument.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levi9.POS.Domain.Models
+{
+    public class ProductDocument
+    {
+        public int ProductId { get; set; }
+        public int DocumentId { get; set; }
+        [Required]
+        [Range(1,int.MaxValue)]
+        public int Quantity { get; set; }
+        [Required]
+        [Range(0, float.MaxValue)]
+        public float Price { get; set; }
+        [Required]
+        [StringLength(3,MinimumLength = 3)]
+        public string Currency { get; set; } =string.Empty;
+
+
+
+        public Product Product { get; set; } = null!;
+        public Document Document { get; set; } = null!;
+
+
+    }
+}
