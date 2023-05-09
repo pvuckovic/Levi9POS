8ab4c21b2e92828c54793c3b7e9fad62a6854c1c
Stevan <61841410+AleteiaSD@users.noreply.github.com>
2023-05-09 15:34:58 +0200

    I have implemented the functionality for searching and filtering products in the following components:
    - ProductController (SearchProducts)
    - IProductRepository (SearchProductsAsync)
    - IProductService (SearchProductsAsync)
    - ProductRepository (implement SearhProductAsync)
    - ProductService (implement SearhProductAsync)
    - ProductSearchRequestDTO
    - ProductSearchRequest
    - ProductSearchResponse
    
    I have also added NUnit tests for the search product functionality in both ProductController and ProductService.

--- Levi9.POS.Domain/DTOs/ProductSearchRequestDTO.cs
+++ Levi9.POS.Domain/DTOs/ProductSearchRequestDTO.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Levi9.POS.Domain.DTOs
+﻿namespace Levi9.POS.Domain.DTOs
 {
     public class ProductSearchRequestDTO
     {
         public int Page { get; set; } = 1;
         public string Name { get; set; }
-        public string OrderBy { get; set; } = "name";
-        public string Direction { get; set; } = "asc";
+        public string OrderBy { get; set; }
+        public string Direction { get; set; }
     }
 }
