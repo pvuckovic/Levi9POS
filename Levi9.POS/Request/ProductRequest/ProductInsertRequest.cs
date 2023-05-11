f844a3de3948dc23e1790e44379739a14644e7e3
Stevan <61841410+AleteiaSD@users.noreply.github.com>
2023-05-11 13:45:15 +0200

    I have added DataAnnotations for validation.

--- Levi9.POS/Request/ProductRequest/ProductInsertRequest.cs
+++ Levi9.POS/Request/ProductRequest/ProductInsertRequest.cs
@@ -3,12 +3,17 @@ namespace Levi9.POS.WebApi.Request.ProductRequest
 {
     public class ProductInsertRequest
     {
+        [Required]
         public Guid GlobalId { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         public string ProductImageUrl { get; set; }
+        [Required]
         public int AvailableQuantity { get; set; }
+        [Required]
         public string LastUpdate { get; set; }
+        [Required]
         public float Price { get; set; }
     }
 }
--- Levi9.POS/Request/ProductRequest/ProductUpdateRequest.cs
+++ Levi9.POS/Request/ProductRequest/ProductUpdateRequest.cs
@@ -3,10 +3,13 @@ namespace Levi9.POS.WebApi.Request.ProductRequest
 {
     public class ProductUpdateRequest
     {
+        [Required]
         public Guid GlobalId { get; set; }
+        [Required]
         public string Name { get; set; }
         public string ProductImageUrl { get; set; }
         public int AvailableQuantity { get; set; }
+        [Required]
         public string LastUpdate { get; set; }
         public float Price { get; set; }
     }
