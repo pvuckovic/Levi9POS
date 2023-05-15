383251619970464b900a2638d9fe016a1ed32788
Aleksandar Lazić <aleksandar.99.lazic@gmail.com>
2023-05-15 16:51:16 +0200

    Pr integration test for get by id document (#15)
    Created integrational tests for get-by-id-document functionality

---
+++ Levi9.POS.IntegrationTests/Controllers/DocumentControllerIntegrationTest.cs
@@ -0,0 +1,111 @@
+﻿using Levi9.POS.Domain;
+using Levi9.POS.Domain.Models;
+using Levi9.POS.IntegrationTests.Fixtures;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace Levi9.POS.IntegrationTests.Controllers
+{
+    [TestFixture]
+    public class DocumentContollerIntegrationTest
+    {
+
+        private WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+        private DataBaseContext _dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // Replace the database context registration with an in-memory database
+                    var descriptor = services.SingleOrDefault(
+                        d => d.ServiceType == typeof(DbContextOptions<DataBaseContext>));
+
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
+
+                    services.AddDbContext<DataBaseContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("TestDatabase");
+                    });
+
+
+                    // Build the service provider to resolve and initialize the database context
+                    var serviceProvider = services.BuildServiceProvider();
+
+                    // Create a new instance of the database context
+                    _dbContext = serviceProvider.GetRequiredService<DataBaseContext>();
+                    _dbContext.Database.EnsureCreated();
+                });
+            });
+
+            // Create an instance of HttpClient for making requests
+            _client = _factory.CreateClient();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up the in-memory database and dispose of resources
+            _dbContext.Database.EnsureDeleted();
+            _dbContext.Dispose();
+            _factory.Dispose();
+            _client.Dispose();
+        }
+
+        [Test]
+        public async Task GetDocumentById_ValidId_ReturnsOk()
+        {
+            // Arrange
+            // Act
+
+            string token = DocumentsFixture.GenerateJwt();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.GetAsync("/v1/document/1");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Document>(content);
+            Assert.NotNull(result);
+            Assert.AreEqual(1, result.Id);
+        }
+
+        [Test]
+        public async Task GetDocumentById_InvalidId_ReturnsBadRequest()
+        {
+            // Arrange
+            // Act
+            string token = DocumentsFixture.GenerateJwt();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.GetAsync("/v1/document/0");
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GetDocumentById_NoUserFound_ReturnsNotFound()
+        {
+            // Arrange
+            // Act
+            string token = DocumentsFixture.GenerateJwt();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.GetAsync("/v1/document/10000");
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+    }
+}
---
+++ Levi9.POS.IntegrationTests/Fixtures/DocumentsFixture.cs
@@ -0,0 +1,59 @@
+﻿using Levi9.POS.Domain.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Levi9.POS.IntegrationTests.Fixtures
+{
+    public static class DocumentsFixture
+    {
+        public static Document GetDataForDocumentRepository()
+        {
+            return new Document()
+            {
+                Id = 1,
+                GlobalId = Guid.NewGuid(),
+                LastUpdate = "133277539861042858",
+                ClientId = 1,
+                DocumentType = "INVOICE",
+                CreationDay = "133277539861042858",
+                ProductDocuments = new List<ProductDocument>()
+                {
+                    new ProductDocument()
+                    {
+                        ProductId = 1,
+                        DocumentId = 1,
+                        Currency = "RSD",
+                        Product = new Product()
+                        {
+                            Id = 1,
+                            Name = "Levi 9 T-Shirt",
+                            GlobalId = Guid.NewGuid(),
+                            ProductImageUrl = "baseURL//nekiurl1.png",
+                            AvailableQuantity = 30,
+                            Price = 60,
+                            LastUpdate = DateTime.Now.AddDays(-1).ToFileTimeUtc().ToString()
+                        },
+                        Price = 1200f,
+                        Quantity = 20
+                    }
+                }
+            };
+        }
+
+        public static string GenerateJwt()
+        {
+            var securityKey = Encoding.UTF8.GetBytes("some-signing-key-here");
+            var symetricKey = new SymmetricSecurityKey(securityKey);
+            var signingCredentials = new SigningCredentials(symetricKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                                            issuer: "http://localhost:5067",
+                                            audience: "http://localhost:5067",
+                                            expires: DateTime.Now.Add(new TimeSpan(86400)),
+                                            signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
